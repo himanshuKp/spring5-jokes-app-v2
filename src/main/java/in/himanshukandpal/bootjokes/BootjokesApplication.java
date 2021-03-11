9ee8318fac9a714bcc0f4ac14ec43eaee213060c
Himanshu Kandpal <cse10324.sbit@gmail.com>
2021-03-11 16:28:35 +0530

    created project

---
+++ src/main/java/in/himanshukandpal/bootjokes/BootjokesApplication.java
@@ -0,0 +1,13 @@
+package in.himanshukandpal.bootjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootjokesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootjokesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/himanshukandpal/bootjokes/BootjokesApplicationTests.java
@@ -0,0 +1,13 @@
+package in.himanshukandpal.bootjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootjokesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
